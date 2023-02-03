2aa2989a314f830057831f8801c9190bc9a6c7a3
Ígor Pedro Leitão Mascarenhas Martins <iplmmartins@ibm.com>
2023-02-03 11:37:55 -0300

    first commit of init project

---
+++ README.md
@@ -0,0 +1,12 @@
+# Create Project API with native lib nodejs
+
+### Run Project
+
+Execute server with restart after change on file
+
+``` node --watch src/server.js``` or ```npm run dev```
+
+### Streams
+
+Streams is to deliver a pieces of content
+Stream [implemented](/streams/fundamentals.js)
---
+++ src/server.js
@@ -0,0 +1,39 @@
+// CommonJS
+// const http = require('');
+
+// ESModule
+// add on file package type: module
+import http from 'node:http';
+
+const users = [];
+
+const server = http.createServer((request, response) => {
+
+    const { method, url } = request;
+
+    if(method === 'GET' && url === '/users') {
+        return response
+            .setHeader('Content-Type', 'application/json')
+            .end(JSON.stringify(users));
+    }
+
+    if(method === 'POST' && url === '/users') {
+
+        users.push({
+            id: 1,
+            name: 'Igor Pedro',
+            email: 'igorpedro@gmail.com'
+        });
+
+        return response
+            .writeHead(201)
+            .end();
+    }
+
+    return response
+        .writeHead(404)
+        .end('Not Found');
+    
+});
+
+server.listen(3333);
