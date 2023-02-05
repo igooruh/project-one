3b18b03c969ef2c6cd1a4d611fd51521292913ec
Ígor Pedro Leitão Mascarenhas Martins <iplmmartins@ibm.com>
2023-02-05 00:15:10 -0300

    body request of stream and buffers in JSON

--- src/server.js
+++ src/server.js
@@ -7,9 +7,21 @@ import http from 'node:http';
 
 const users = [];
 
-const server = http.createServer((request, response) => {
+const server = http.createServer(async (request, response) => {
 
     const { method, url } = request;
+
+    const buffers = [];
+
+    for await (const chunk of request) {
+        buffers.push(chunk);
+    }
+
+    try {
+        request.body = JSON.parse(Buffer.concat(buffers).toString());
+    } catch {
+        request.body = null;
+    }
 
     if(method === 'GET' && url === '/users') {
         return response
@@ -19,10 +31,12 @@ const server = http.createServer((request, response) => {
 
     if(method === 'POST' && url === '/users') {
 
+        const { name, email } = request.body;
+
         users.push({
             id: 1,
-            name: 'Igor Pedro',
-            email: 'igorpedro@gmail.com'
+            name,
+            email
         });
 
         return response
