30002912cfc7b35e0aa1b830b65dfcfa4c1c7b74
Ígor Pedro Leitão Mascarenhas Martins <iplmmartins@ibm.com>
2023-02-05 13:59:46 -0300

    feat: create persistence on file database

--- src/database.js
+++ src/database.js
@@ -1,6 +1,23 @@
+import fs from 'node:fs/promises';
+
+const databasePath = new URL('../db.json', import.meta.url);
+
 export class Database {
     #database = {};
-    _igor = {}
+
+    constructor() {
+        fs.readFile(databasePath, 'utf8')
+          .then(data => {
+            this.#database = JSON.parse(data);
+          })
+          .catch(() => {
+            this.#persist();
+          });
+    }
+
+    #persist() {
+        fs.writeFile(databasePath, JSON.stringify(this.#database));
+    }
 
     select(nameTable) {
         const data = this.#database[nameTable] ?? [];
@@ -14,6 +31,8 @@ export class Database {
         } else {
             this.#database[nameTable] = data;
         }
+
+        this.#persist();
 
         return data;
     }
